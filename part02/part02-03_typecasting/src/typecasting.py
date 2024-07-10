60e3de8871ca2f55129cb445c7e87ae410353ff6
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2024-07-10 04:43:00 -0400

    Add files via upload

---
+++ part02/part02-03_typecasting/src/typecasting.py
@@ -0,0 +1,8 @@
+# Write your solution here
+
+number = float(input("Please type in a number:"))
+int_part = int(number)
+dec_part = number - int_part
+
+print("Integer part:", int_part)
+print("Decimal part:", dec_part)
