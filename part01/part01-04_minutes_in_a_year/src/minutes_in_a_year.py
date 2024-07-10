220a49f98454399f51ced192c8299c8ec7494416
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2024-07-10 04:32:23 -0400

    Add files via upload

---
+++ part01/part01-04_minutes_in_a_year/src/minutes_in_a_year.py
@@ -0,0 +1,4 @@
+# Write your solution here
+
+print("number of minutes in a year:")
+print(365*24*60) 
---
+++ part01/part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,9 @@
+# Write your solution here
+
+name = input("Given name:")
+family = input("Family name:")
+address = input("Street Address:")
+city_postal = input("City and Postal code:")
+print(name + " " + family)
+print(address)
+print(city_postal)
