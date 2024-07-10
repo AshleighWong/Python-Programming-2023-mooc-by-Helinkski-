ecbc26dcc0addbcfd69ea5217a701e49b0d431ef
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2024-07-10 04:33:01 -0400

    Add files via upload

---
+++ part01/part01-10b_extra_space/src/extra_space.py
@@ -0,0 +1,18 @@
+
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print(f"my name is {name}, I am {age} years old", "\n")
+print("my skills are")
+print(" -", skill1, f"({level1})")
+print(" -", skill2, f"({level2})")
+print(" -", skill3, f"({level3})", "\n")
+print("I am looking for a job with a salary of", f"{lower}-{upper}", "euros per month")
---
+++ part01/part01-11_arithmetics/src/arithmetics.py
@@ -0,0 +1,13 @@
+# Write your solution here
+x = 27
+y = 15
+
+add = x + y 
+sub = x - y
+multi = x * y
+div = x / y
+
+print(f"{x} + {y} = {add}")
+print(f"{x} - {y} = {sub}")
+print(f"{x} * {y} = {multi}")
+print(f"{x} / {y} = {div}")
